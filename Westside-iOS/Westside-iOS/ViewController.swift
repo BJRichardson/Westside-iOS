7d92793f9a043ec83a771ce42360a1d3a008299a
Bryan Richardson <bryanrichardson@bryans-mbp.attlocal.net>
2017-10-30 13:34:25 -0400

    Adds splash screen

--- Westside-iOS/Westside-iOS/ViewController.swift
+++ Westside-iOS/Westside-iOS/ViewController.swift
@@ -1,7 +1,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var overlayView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
