e9e4b01deb3e2de0c93f00e94a73226dccaddd12
Bryan Richardson <bryanrichardson@bryans-mbp.attlocal.net>
2017-11-01 16:53:11 -0400

    Begins Store implementation

--- Westside-iOS/Westside-iOS/Store/Store.swift
+++ Westside-iOS/Westside-iOS/Store/Store.swift
@@ -1,9 +1,32 @@
 import Foundation
+import CoreData
 
 class Store: NSObject {
     
     static let sharedInstance = Store()
-    
     static let resourceEndpoint = "https://westsidecme.herokuapp.com/"
     
+    static let serverOverrideKey = "serverOverrideURL"
+    static let tokenKey = "token"
+    
+    enum Environment: String {
+        case staging
+        case production
+        
+        var url: URL {
+            switch self {
+            case .production:
+                return URL(string: "https://westsidecme.herokuapp.com/")!
+            case .staging:
+                return URL(string: "https://westsidecme.herokuapp.com/")!
+            }
+        }
+        
+        var clientId: String {
+            return "com.westside.backend"
+        }
+        var clientSecret: String {
+            return "fellowship1953"
+        }
+    }
 }
